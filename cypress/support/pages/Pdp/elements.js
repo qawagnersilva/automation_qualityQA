ddb4d95802e85f573d89252bd08cb967c30cc718
Wagner <wag.gomes2015@gmail.com>
2023-05-30 21:21:16 -0300

    Desafio 4 de automação

---
+++ cypress/support/pages/Pdp/elements.js
@@ -0,0 +1,17 @@
+export const ELEMENTS = {
+    //Seleção de elemento pela classe CSS
+    imagemProduct: '.view-product',
+
+    //Seleção de elemento por xPath
+    btnAddToCart: '/html/body/section/div/div/div[2]/div[2]/div[2]/div/span/button',
+
+    //Seleção de elemento por id
+    inputName: '#name',
+    inputEmail: '#email',
+    btnSubmit: '#button-review',
+
+    //Seleção de elemento por atributo HTML
+    textAreaReview: '[placeholder="Add Review Here!"]',
+    inputQuantity: '[name="quantity"]'
+
+}
